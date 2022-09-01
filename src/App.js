32454d2707e0612dac6f553bead1c48308f8f0b0
sas7359 <sas7359@rit.edu>
2022-08-31 20:31:20 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://www.se.rit.edu/resources/images/se_logo_2021.png" className="App-logo" alt="logo" />
+        <p>
+          Soon to be Anti-Pattern Intellij Plugin Project Team Website!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
