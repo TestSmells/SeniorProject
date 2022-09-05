63cfed41dbd9be54952ed5e3fbb30e3d2f2a5258
sas7359 <sas7359@g.rit.edu>
2022-09-05 15:38:49 -0400

    second fix attempt for about us page

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <Container>
             <Nav className="me-auto">
               <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/about">About Us</Nav.Link>
+              <Nav.Link href="/#/about">About Us</Nav.Link>
             </Nav>
         </Container>
       </Navbar>
