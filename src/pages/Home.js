7518a68fee3334c66b812be09312c7ade3c9208e
sas7359 <sas7359@g.rit.edu>
2022-09-09 11:21:28 -0400

    update 4up for week of September 5th

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,6 @@ const Home = () => {
             <Row>
                 <h1>TS-DETECT Anti-Pattern Detection IntelliJ Plugin</h1>
                 <p>Project description, etc.</p> 
-                <a href="https://docs.google.com/spreadsheets/d/18-L8U9jV1wC0lBaZethwOkAlIVz_PU4vnbdsJsMclNw/edit?usp=sharing">Team Time Tracking Spreadsheet</a> 
             </Row>
         </>
     )
