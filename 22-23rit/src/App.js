2005ead58d1b3286f779d7908fe3b33acd66ce65
sas7359 <sas7359@g.rit.edu>
2022-09-14 20:08:45 -0400

    add notfound page

--- 22-23rit/src/App.js
+++ 22-23rit/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Weekly from './pages/Weekly';
+import NotFound from './pages/NotFound';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 function App() {
@@ -18,9 +19,10 @@ function App() {
       </Navbar>
       <Container className="mt-10">
         <Routes>
-            <Route path="/" element={<Home />} />
+            <Route exact path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
-            <Route path="/weekly" element={<Weekly />}></Route>
+            <Route path="/weekly" element={<Weekly />} />
+            <Route path="*" element={<NotFound />} />
         </Routes>
       </Container>
     </div>
---
+++ 22-23rit/src/pages/NotFound.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+import { Row } from "react-bootstrap";
+
+const NotFound = () => {
+    return (
+        <>
+            <Row>
+                <h1>404 Not Found</h1>
+            </Row>
+        </>
+    )
+}
+
+export default NotFound;
